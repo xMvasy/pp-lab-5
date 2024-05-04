5e82503c7b708be12c4acc0b67b98ee415a7484f
Maria Garlińska <162016875+xMvasy@users.noreply.github.com>
2024-05-04 11:35:34 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(123, 532);
+        int result = MathUtils.add(123, 456);
         System.out.println("Wynik sumy dwóch liczb 123 i 456:  " + result);
 
         Messenger messenger = new EmailMessenger();
